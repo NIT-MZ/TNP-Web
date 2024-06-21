c023da452d82502bea693d983d4ddf97167cae39
AdityacodeNIT <srivastavaditya145@gmail.com>
2024-06-21 17:40:51 +0530

    counts

--- src/components/Counts.js
+++ src/components/Counts.js
@@ -1,58 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stylesheets/Counts.scss";
-import { useContext } from "react";
-import UserContext from "../context/UserContext";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const Counts = () => {
-  const { count } = useContext(UserContext);
+  const [counterOn, setCounterOn] = useState(false);
 
   return (
     <section id="counts" className="counts section-bg ">
-      <div className="container">
-        <div className="row justify-content-end">
-          <div
-            className="col-lg-3 col-md-5 col-6 d-md-flex
+      <ScrollTrigger
+        onEnter={() => setCounterOn(true)}
+        onExit={() => setCounterOn(false)}
+      >
+        <div className="container">
+          <div className="row justify-content-end">
+            <div
+              className="col-lg-3 col-md-5 col-6 d-md-flex
               align-items-md-stretch"
-          >
-            <div className="count-box">
-              <span>{count[0]}%</span>
-              <p>Students Placed in Session 2023-24</p>
+            >
+              <div className="count-box">
+                <span>
+                  {" "}
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={86}
+                      duration={2}
+                      delay={0}
+                      decimals={1}
+                      suffix="%"
+                    ></CountUp>
+                  )}
+                </span>
+                <p>Students Placed in Session 2023-24</p>
+              </div>
             </div>
-          </div>
 
-          <div
-            className="col-lg-3 col-md-5 col-6 d-md-flex
+            <div
+              className="col-lg-3 col-md-5 col-6 d-md-flex
               align-items-md-stretch"
-          >
-            <div className="count-box">
-              <span>{count[1]}</span>
-              <p>Students Completed their Internship & Summer Training</p>
+            >
+              <div className="count-box">
+                <span>
+                  {" "}
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={90}
+                      duration={2}
+                      delay={0}
+                      suffix="%"
+                    ></CountUp>
+                  )}
+                </span>
+                <p>Students Completed their Internship & Summer Training</p>
+              </div>
             </div>
-          </div>
 
-          <div
-            className="col-lg-3 col-md-5 col-6 d-md-flex
+            <div
+              className="col-lg-3 col-md-5 col-6 d-md-flex
               align-items-md-stretch"
-          >
-            <div className="count-box">
-              <span>{count[2]}</span>
-              <p>Highest Package of B.Tech. Students (in LPA)</p>
+            >
+              <div className="count-box">
+                <span>
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={14.5}
+                      duration={2}
+                      delay={0}
+                      decimals={1}
+                    ></CountUp>
+                  )}
+                </span>
+                <p>Highest Package of B.Tech. Students (in LPA)</p>
+              </div>
             </div>
-          </div>
 
-          <div
-            className="col-lg-3 col-md-5 col-6 d-md-flex
+            <div
+              className="col-lg-3 col-md-5 col-6 d-md-flex
               align-items-md-stretch"
-          >
-            <div className="count-box">
-              <span>{count[3]}</span>
-              <a href="https://NIT Mizoram.ac.in/mous/">
-                <p>MoU & Training Programs Signed with Other Institutions</p>
-              </a>
+            >
+              <div className="count-box">
+                <span>
+                  {" "}
+                  {counterOn && (
+                    <CountUp
+                      start={0}
+                      end={19}
+                      duration={2}
+                      delay={0}
+                    ></CountUp>
+                  )}
+                </span>
+                <a href="https://NIT Mizoram.ac.in/mous/">
+                  <p>MoU & Training Programs Signed with Other Institutions</p>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </ScrollTrigger>
     </section>
   );
 };
--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,40 +1,42 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 import UserContext from "./UserContext.js";
 
 const UserContextProvider = ({ children }) => {
-  const [count, setCount] = useState(new Array(4).fill(0)); // Initialize count as an array
-  const list = [86, 96, 14.5, 19]; // List of target values
+  // const [count, setCount] = useState(new Array(4).fill(0)); // Initialize count as an array
+  // const list = [86, 96, 14.5, 19]; // List of target values
 
-  useEffect(() => {
-    const timers = [];
+  // useEffect(() => {
+  //   const timers = [];
 
-    list.forEach((value, index) => {
-      if (count[index] < value) {
-        const timer = setInterval(() => {
-          setCount(prevCount => {
-            const newCount = [...prevCount];
-            const step = value > 1 ? 1 : 0.1; // Determine the step size based on the value
-            newCount[index] = parseFloat((newCount[index] + step).toFixed(1)); // Increment and ensure one decimal place
+  //   list.forEach((value, index) => {
+  //     if (count[index] < value) {
+  //       const timer = setInterval(() => {
+  //         setCount(prevCount => {
+  //           const newCount = [...prevCount];
+  //           const step = value > 1 ? 1 : 0.1; // Determine the step size based on the value
+  //           newCount[index] = parseFloat((newCount[index] + step).toFixed(1)); // Increment and ensure one decimal place
 
-            // Check if reached or exceeded the target value, then clear the interval
-            if (newCount[index] >= value) {
-              clearInterval(timer);
-              newCount[index] = value; // Ensure exact target value
-            }
+  //           // Check if reached or exceeded the target value, then clear the interval
+  //           if (newCount[index] >= value) {
+  //             clearInterval(timer);
+  //             newCount[index] = value; // Ensure exact target value
+  //           }
 
-            return newCount;
-          });
-        }, 20); // Adjust the interval as needed
+  //           return newCount;
+  //         });
+  //       }, 20); // Adjust the interval as needed
 
-        timers.push(timer);
-      }
-    });
+  //       timers.push(timer);
+  //     }
+  //   });
 
-    return () => timers.forEach(timer => clearInterval(timer)); // Clean up all intervals
-  }, [count, list]);
+  //   return () => timers.forEach(timer => clearInterval(timer)); // Clean up all intervals
+  // }, [count, list]);
+
+ // <UserContext.Provider value={{ count }}>
 
   return (
-    <UserContext.Provider value={{ count }}>
+    <UserContext.Provider value={{  }}>
       {children}
     </UserContext.Provider>
   );
