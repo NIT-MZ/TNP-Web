07712166453b87c63e19e9b467fd1a592f63b92b
AdityacodeNIT <srivastavaditya145@gmail.com>
2025-06-02 11:22:53 +0530

    fix: comment out WebTeam component in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
       <Team />
       <TrainingandRecruitingPartners />
       <TestimonialSection />
-      <WebTeam />
+      {/* <WebTeam /> */}
       <CTAsection />
       <Contact />
       <Map />
